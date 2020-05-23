a9d2e7b3089a3af87174e34d45ebe06e1022517d
RiccardoCampitelli <riccardo.campitelli91@gmail.com>
2020-05-23 19:01:29 +0100

    add port to template initial log

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { Application } from "express";
 
 const app: Application = express();
 
+const port = 5000;
+
 const exampleJson = {
   value: "test",
   arrayOfValues: ["one", "two", "three"]
@@ -11,4 +13,4 @@ app.get("/", (req, res) => {
   res.send(exampleJson);
 });
 
-app.listen(5000, () => console.log("server running"));
+app.listen(port, () => console.log(`listening on port: ${port}`));
